9b7b6707fb42062ea331e0e8455ba78eaf103688
Colby Stone <colbyjstone@gmail.com>
2025-02-25 08:31:01 -0500

    Initialize Next.js application

---
+++ next-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// It's automatically generated 
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+  }
+}
